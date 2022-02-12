6d2f07b2343e115712a5001ddab282658ba086d9
Whitley Horn <whitleyblaine@protonmail.com>
2022-02-11 21:27:58 -0500

    Create our pages, our Page component, our _app.jsx and our _document.jsx

---
+++ frontend/components/Page.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <h2>I am the page component</h2>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
+
+Page.defaultProps = {
+  children: [],
+};
